58ed37c90f361e1ca4633bdded3e185ffc6117dc
王德强 <a14907@outlook.com>
2020-10-28 19:36:27 +0800

    fix

--- ProxyRequest/Controllers/HomeController.cs
+++ ProxyRequest/Controllers/HomeController.cs
@@ -24,17 +24,17 @@ namespace ProxyRequest.Controllers
         [HttpGet("~/")]
         public string Index()
         {
-            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff" + "(tubi=youtube,string scheme, string domain, string path, string param)");
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff" + "(tubi=youtube,string scheme, string domain, string path, string query)");
         }
 
         [HttpGet("~/proxy")]
-        public async Task<IActionResult> ProxyAsync(string scheme, string domain, string path, string param)
+        public async Task<IActionResult> ProxyAsync(string scheme, string domain, string path, string query)
         {
             if (string.Equals(domain, "tubi", StringComparison.OrdinalIgnoreCase))
             {
                 domain = "www.youtube.com";
             }
-            var url = $"{scheme}://{domain}{path}?{param}";
+            var url = $"{scheme}://{domain}{path}?{query}";
             var response = await _client.GetAsync(url);
             var body = await response.Content.ReadAsStringAsync();
             _logger.LogWarning($"当前请求地址：{url}，响应code：{response.StatusCode}，响应body长度：{body.Length}");
